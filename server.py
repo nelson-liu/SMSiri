87076c1b8caa526606b49a0e40aacec882332e66
Nelson Liu <nelson.liu.2009@gmail.com>
2015-09-12 03:34:53 -0400

    reformed the callWit method to a general receiveSMS method that decides what helper functions to call

--- server.py
+++ server.py
@@ -1,23 +1,46 @@
 from flask import Flask, request, redirect
 import requests
 import json
+import random
 import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def callWit():
-    """Respond to a text with the input, and print the wit.ai generated json to console"""
+noIntent = [
+    "I'm having trouble understanding you, could you rephrase your question?",
+    "I didn't catch that, could you rephrase your query?",
+    "Sorry, I didn't understand that. Try rephrasing your request."
+]
 
+@app.route("/", methods=['GET', 'POST'])
+def recieveSMS():
+    """Respond to a text with the input (this is a temporary behavior), and decide the appropriate handler"""
     text_body = request.values.get('Body', None)
-    resp = twilio.twiml.Response()
-    resp.message(text_body)
+    # resp = twilio.twiml.Response()
+    # resp.message(text_body)
     wit_response = requests.get(url='https://api.wit.ai/message?v=20150912&q=' + request.values.get('Body', None),headers={'Authorization': 'Bearer I4WKESB35IVVAHPAG4YVYRQ6MB26UAGG'})
     wit_dict = json.loads(wit_response.text)
     print wit_dict
-    return str(resp)
+    intent = wit_dict.get('outcomes')[0].get('intent')
+    print intent
+    entities = wit_dict.get('outcomes')[0].get('entities')
+    print entities
 
-=======
+    if intent == "lookup":
+        lookup(entities)
+    elif intent == "navigate":
+        navigate(entities)
+    elif intent == "translate":
+        translate(entities)
+    elif intent == "weather":
+        weather(entities)
+    elif intent == "twitter_updates":
+        twitter_updates(entities)
+    elif intent == "stock_report":
+        stock_report(entities)
+    else:
+        noValidIntent()
+
 #1 Lookup uses the Bing Search API
 @app.route("/lookup", methods=['GET', 'POST'])
 def lookup(query, search_type):
@@ -32,7 +55,7 @@ def lookup(query, search_type):
     #request.add_header('Authorization', auth)
     #request.add_header('User-Agent', user_agent)
     request_opener = urllib2.build_opener()
-    response = request_opener.open(request) 
+    response = request_opener.open(request)
     response_data = response.read()
     json_result = json.loads(response_data)
     result_list = json_result['d']['results']
@@ -41,9 +64,14 @@ def lookup(query, search_type):
 
 #2 Navigate
 @app.route("/navigate", methods=['GET', 'POST'])
-def navigate(x, y): 
+def navigate(x, y):
     return -1 #TODO
->>>>>>> origin/master
+
+# No Valid Intent Found
+@app.route("/noValidIntent", methods=['GET', 'POST'])
+def noValidIntent():
+    resp = twilio.twiml.Response()
+    resp.message(random.choice(noIntent))
 
 if __name__ == "__main__":
     app.run(debug=True)
