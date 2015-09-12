728e0396945fe273d279ac37bb4a773f34169139
Nelson Liu <nelson.liu.2009@gmail.com>
2015-09-12 02:48:15 -0400

    basic usage of wit.ai with requests library

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, redirect
+import requests
+import json
 import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+def callWit():
+    """Respond to a text with the input, and print the wit.ai generated json to console"""
 
+    text_body = request.values.get('Body', None)
     resp = twilio.twiml.Response()
-    with resp.message("Hello, Mobile Monkey") as m:
-        m.media("https://demo.twilio.com/owl.png")
+    resp.message(text_body)
+    wit_response = requests.get(url='https://api.wit.ai/message?v=20150912&q=' + request.values.get('Body', None),headers={'Authorization': 'Bearer I4WKESB35IVVAHPAG4YVYRQ6MB26UAGG'})
+    wit_dict = json.loads(wit_response.text)
+    print wit_dict
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
