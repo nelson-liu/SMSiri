d69b04122137c3cad6d7e3bbf17dd4a93d029447
Matthew Allen Lin <Darthpwner@587214559.guest.umich.net>
2015-09-12 01:20:59 -0400

    Added the Bing Search API rough algorithm, refine it by removing authentication (not necessary for us)

--- server.py
+++ server.py
@@ -12,5 +12,25 @@ def hello_monkey():
         m.media("https://demo.twilio.com/owl.png")
     return str(resp)
 
+#1 Lookup uses the Bing Search API
+def lookup(query, search_type):
+    #search_type: Web, Image, News, Video
+    key = nAiE8uvJl0LDZE0U0rqvxcIt93KFjmLcyiDF3jpk8ig
+    # create credential for authentication
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+    credentials = (':%s' % key).encode('base64')[:-1]
+    auth = 'Basic %s' % credentials
+    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=5&$format=json'
+    request = urllib2.Request(url)
+    request.add_header('Authorization', auth)
+    request.add_header('User-Agent', user_agent)
+    request_opener = urllib2.build_opener()
+    response = request_opener.open(request) 
+    response_data = response.read()
+    json_result = json.loads(response_data)
+    result_list = json_result['d']['results']
+    print result_list
+    return result_list
+
 if __name__ == "__main__":
     app.run(debug=True)
