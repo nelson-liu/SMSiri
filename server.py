663067182c986c6e8b51e3269c8edcf380ea7b7c
Matthew Allen Lin <Darthpwner@587214559.guest.umich.net>
2015-09-12 03:40:42 -0400

    Cleaned up comments and renamed the parameters of each function to 'entities'

--- server.py
+++ server.py
@@ -43,17 +43,10 @@ def recieveSMS():
 
 #1 Lookup uses the Bing Search API
 @app.route("/lookup", methods=['GET', 'POST'])
-def lookup(query):
-    #search_type: Web, Image, News, Video
+def lookup(entities):
     key = nAiE8uvJl0LDZE0U0rqvxcIt93KFjmLcyiDF3jpk8ig
-    # create credential for authentication
-    #user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
-    #credentials = (':%s' % key).encode('base64')[:-1]
-    #auth = 'Basic %s' % credentials
-    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+'?Query=%27'+query+'%27&$top=5&$format=json'
+    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+'?Query=%27'+entities+'%27&$top=5&$format=json'
     request = urllib2.Request(url)
-    #request.add_header('Authorization', auth)
-    #request.add_header('User-Agent', user_agent)
     request_opener = urllib2.build_opener()
     response = request_opener.open(request)
     response_data = response.read()
@@ -64,7 +57,7 @@ def lookup(query):
 
 #2 Navigate
 @app.route("/navigate", methods=['GET', 'POST'])
-def navigate(x, y):
+def navigate(entities):
     return -1 #TODO
 
 # No Valid Intent Found
